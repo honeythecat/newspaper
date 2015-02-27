7b563d71125ccb9744354a9eebabee14fad8cb38
Heather <heather@thisisnotanorg.org>
2015-02-27 10:21:07 -0800

    Add basic working homepage and fix broken links.

---
+++ app.js
@@ -0,0 +1,22 @@
+var newspaper = angular.module('newspaper', ['ui.router']);
+
+newspaper.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+//
+//   $stateProvider.state('beats', {
+//     url: "/beats",
+//     templateUrl: "partials/beats.html"
+//     controller: "BeatsCtrl"
+//   });
+//
+//   $stateProvider.state('beats.reporters', {
+//     url: "/:beatsId",
+//     templateUrl: "partials/beats.reporters.html",
+//     controller: "ReportersCtrl"
+//   });
+//
+//
+});
